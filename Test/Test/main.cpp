070b07c3837c452c5451e06ceb135544f0549e48
Galla Syrodoeva <55047006+gallasglasses@users.noreply.github.com>
2025-08-25 15:03:10 +0200

    Hello Window

---
+++ Test/Test/main.cpp
@@ -0,0 +1,66 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+const unsigned int SCREEN_WIDHT = 800;
+const unsigned int SCREEN_HEIGHT = 600;
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDHT, SCREEN_HEIGHT, "LearnOpenGl", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to creat GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Render loop
+    while (!glfwWindowShouldClose(window))
+    {
+        //Input
+        processInput(window);
+
+        // Rendering commands here
+        
+        glClearColor(0.2f, 0.08f, 0.2f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // ^^^^^^
+
+        // Check and call events and swap the buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height); // first two parameters set the location of the lower left corner
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
